7875e31c4cd6e7fe5841da06666b03a63d3cd252
Agustin Pontura <pontura@gmail.com>
2020-04-02 11:38:29 -0300

    2 players

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -8,32 +8,69 @@ public class Ball : MonoBehaviour
     float direction_x = 1;
     float direction_y = 1;
 
+    public Player player1;
+    public Player player2;
+
     public Game game;
+    float _x;
+    float _y;
 
     void Update()
     {
-        float _x = transform.position.x + speed * direction_x;
-        float _y = transform.position.y + speed * direction_y;
+        _x = transform.position.x + speed * direction_x * Time.deltaTime;
+        _y = transform.position.y + speed * direction_y * Time.deltaTime;
 
         if (_x >= game.sizes.x)
-        {
-            _x = game.sizes.x;
-            direction_x = direction_x * -1;
-        }else if(_x <= -game.sizes.x)
-        {
-            _x = -game.sizes.x;
-            direction_x = direction_x * -1;
-        }
+            ChangeXDirection(1);
+        else if(_x <= -game.sizes.x)
+            ChangeXDirection(-1);
+
         if (_y >= game.sizes.y)
-        {
-            _y = game.sizes.y;
-            direction_y = direction_y * -1;
-        } else if (_y <= -game.sizes.y)
-        {
-            _y = -game.sizes.y;
-            direction_y = direction_y * -1;
-        }
+            ChangeYDirection(1);
+        else if (_y <= -game.sizes.y)
+            ChangeYDirection(-1);
 
         transform.position = new Vector2(_x, _y);
+    }
+    private void ChangeXDirection(int direction)
+    {
+        //cuando direction = 1 tengo que chequear sobre la posicion en y del player 1
+        //cuando direction = -1 tengo que chequear sobre la posicion en y del player 2
+        if (direction==1)
+        {
+            bool hitted = CheckCollisionTo(player1);
+            if (hitted)
+                print("sigue");
+            else
+                print("game over");
+        } else
+        {
+            bool hitted = CheckCollisionTo(player2);
+            if (hitted)
+                print("sigue");
+            else
+                print("game over");
+        }
+        _x = game.sizes.x * direction;
+        direction_x *= -1;
+    }
+
+    private bool CheckCollisionTo(Player player)
+    {
+        if (
+            (_y < player.transform.position.y + (player.transform.localScale.y / 2)) 
+            && 
+            (_y > player.transform.position.y - (player.transform.localScale.y / 2))
+            )
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    private void ChangeYDirection(int direction)
+    {
+        _y = game.sizes.y * direction;
+        direction_y *= -1;
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 0.8f;
+    public Game game;
+    public types type;
+    public enum types
     {
-        
+        PLAYER1,
+        PLAYER2
     }
-
-    // Update is called once per frame
+    float offset = 0.7f;
     void Update()
     {
-        
+        float player_y = transform.position.y;
+        if (type == types.PLAYER1)
+        {
+            if (Input.GetKey(KeyCode.W) && player_y <= game.sizes.y - offset)
+            {
+                Move(1);
+            }
+            if (Input.GetKey(KeyCode.S) && player_y >= -game.sizes.y + offset)
+            {
+                Move(-1);
+            }
+        }
+        if (type == types.PLAYER2)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && player_y <= game.sizes.y - offset)
+            {
+                Move(1);
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && player_y >= -game.sizes.y + offset)
+            {
+                Move(-1);
+            }
+        }
+    }
+    void Move(int direction)
+    {        
+        transform.position = new Vector2(transform.position.x, transform.position.y + speed * direction * Time.deltaTime);
     }
 }
